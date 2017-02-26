ce0f6cbf9d014b7edbffd9c9ba0875bb9bf68642
Kiwi Liu <kiwi71728@icloud.com>
2017-02-26 09:36:55 +0800

    Bash prompt with npm version prefix is implemented and fix some bugs.

--- trigger_nvm_use.sh
+++ trigger_nvm_use.sh
@@ -1,21 +1,59 @@
 #!/bin/bash
 
-triggerNvmUse() {
-    config="$HOME/.currentNvm"
+NPM_VERSION_PROMPT_SYMBOL="Ⓝ"
+NPM_PROMPT_PREFIX="$NPM_VERSION_PROMPT_SYMBOL REPLACE_VAL "
+
+nvmVersionPrompt_addPrompt() {
+    local prompt
+
+    prompt=$(nvmVersionPrompt_getPrompt "$1")
+
+    if [[ ! $PS1 == *$NPM_VERSION_PROMPT_SYMBOL* ]]; then
+        PS1="$prompt$PS1"
+    elif [[ ! $PS1 == *$prompt* ]]; then
+        PS1=$(echo -n "$PS1" | sed -r 's/'"$NPM_VERSION_PROMPT_SYMBOL"' v([[:digit:]]+\.){2}[[:digit:]]+ //')
+        PS1="$prompt$PS1"        
+    fi
+}
+
+nvmVersionPrompt_getPrompt() {
+    echo "${NPM_PROMPT_PREFIX/REPLACE_VAL/$1}"
+}
+
+nvmVersionPrompt_trigger() {
+    local nvmCurrent
+    local nvmrc
+
+    nvmCurrent=$(nvm current)
     nvmrc="${PWD}/.nvmrc"
-    if ! [[ -e "$nvmrc" ]]; then return 1; fi
-    if [[ -e $config && $(nvm which) == *$(cat "$config")* ]]; then return 1; fi
+
+    if ! [[ -e "$nvmrc" ]]; then 
+        nvmVersionPrompt_addPrompt "$nvmCurrent"
+        return 1
+    fi
+    if [[ $(nvm which) == *$nvmCurrent* ]]; then 
+        nvmVersionPrompt_addPrompt "$nvmCurrent"
+        return 1 
+    fi
+
     nvm use
-    echo -n "$(nvm current)" > "$config"
+
+    nvmCurrent=$(nvm current)
+    nvmVersionPrompt_addPrompt "$nvmCurrent"
+    nvmVersionPrompt_addCommand "nvmVersionPrompt_trigger"
     return 0
 }
 
-if [[ ! $PROMPT_COMMAND == *triggerNvmUse\;* ]]
-then
-    prefix=""
-    if [[ -n $PROMPT_COMMAND && ! $PROMPT_COMMAND == *\; ]]
+nvmVersionPrompt_addCommand() {
+    if [[ ! $PROMPT_COMMAND == *$1\;* ]]
     then
-        prefix=";"
+        prefix=""
+        if [[ -n $PROMPT_COMMAND && ! $PROMPT_COMMAND == *\; ]]
+        then
+            prefix=";"
+        fi
+        PROMPT_COMMAND=$PROMPT_COMMAND$prefix"$1;"
     fi
-    PROMPT_COMMAND=$PROMPT_COMMAND$prefix"triggerNvmUse;"
-fi
+}
+
+nvmVersionPrompt_addCommand "nvmVersionPrompt_trigger"
